9656566e0cdec167e56707ac6f540dcdb222e793
priyankanadar17 <42005050+priyankanadar17@users.noreply.github.com>
2020-07-13 22:48:31 +0530

    Add files via upload

---
+++ day2/first.js
@@ -0,0 +1,5 @@
+function currentdate()
+                {
+                    document.getElementById('sample').innerHTML='It is '+curr+'/'+curr1+'/'+curr2+'.';
+                }
+                
